e82d143fdef27db7e828426a4c632bc3ae001eb5
ndzd <n_level@mail.ru>
2022-10-05 10:26:04 +0300

    T2

---
+++ Task2/Program.cs
@@ -0,0 +1,15 @@
+﻿// Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+//Console.Clear();
+
+int GetResult(int M, int N){
+    
+    int result = 0;
+    if(N == M) return result+N;
+    
+    return N + GetResult(M, N - 1); 
+    }
+
+    System.Console.WriteLine(GetResult(4,8));
